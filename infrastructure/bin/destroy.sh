7d626aef4699aec00d6b5f9ddb9f1f8847a471cd
Alican <aerdogan07@gmail.com>
2020-03-14 16:41:37 +0100

    Clean up for the scripts

--- infrastructure/bin/destroy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-BASEDIR=$(dirname "$0")
-cd $BASEDIR/..
-
-(cd "env/staging/server" && terraform destroy -auto-approve)
-(cd "env/staging/db" && terraform destroy -var "db_password=$DB_PASSWORD" -var "db_username=$DB_USERNAME" -auto-approve)
-(cd "env/staging/cdn" && terraform destroy -auto-approve)
-(cd "env/staging/network" && terraform destroy -auto-approve)
