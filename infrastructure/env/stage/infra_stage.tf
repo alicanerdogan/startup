27a9780fef1c35fd7aef8e94803a4cd022eac08f
Alican <aerdogan07@gmail.com>
2020-03-28 18:38:47 +0100

    Fixed repeated output

--- infrastructure/env/stage/infra_stage.tf
+++ infrastructure/env/stage/infra_stage.tf
@@ -136,11 +136,6 @@ output "s3_id" {
   description = "The id of the S3 bucket"
 }
 
-output "s3_id" {
-  value       = module.resources.s3_id
-  description = "The id of the S3 bucket"
-}
-
 output "s3_website_endpoint" {
   value       = module.resources.s3_website_endpoint
   description = "The S3 website endpoint"
